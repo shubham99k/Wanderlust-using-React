25f8b435386e991c1e3327195c88efcd6eab6774
shubham99k <ssali71173@gmail.com>
2026-03-06 15:11:41 +0530

    Update seed script to use Atlas DB URL

--- backend/init/index.js
+++ backend/init/index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose"); // MongoDB object modeling library
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+
+const dbUrl = process.env.ATLASDB_URL || 'mongodb://127.0.0.1:27017/wanderlust';
 
 main()
     .then(() => {
@@ -9,12 +12,12 @@ main()
     .catch(err => console.log(err)); // Log connection error if failed
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust'); // Database name: wanderlust
+    await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "6993006094eb8ec198c65fd7"}));
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "6993006094eb8ec198c65fd7" }));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
