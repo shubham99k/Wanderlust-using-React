05a825971865942ec885bbf229fc6f3018f8e2a9
shubham99k <ssali71173@gmail.com>
2026-03-06 15:26:00 +0530

    update init folder

--- backend/init/index.js
+++ backend/init/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose"); // MongoDB object modeling library
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const dbUrl = process.env.ATLASDB_URL || 'mongodb://127.0.0.1:27017/wanderlust';
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
     .then(() => {
